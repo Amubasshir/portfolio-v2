9574e761a017a039e7cdafe798e41ae4178d8ce2
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-02 14:42:25 +0600

    about p added

--- pages/about.js
+++ pages/about.js
@@ -21,23 +21,22 @@ const about = () => {
                 ABOUT ME
               </h2>
               <p>
-                Hi, I'm CodeBucks, a web developer and UI/UX designer with a
-                passion for creating beautiful, functional, and user-centered
-                digital experiences. With 4 years of experience in the field. I
-                am always looking for new and innovative ways to bring my
-                clients' visions to life.
+                Hi, I'm Mubasshir, a react developer with a passion for creating
+                beautiful, functional, and user-centered digital experiences.
+                With one year of experience in the field. I am always looking
+                for new and innovative ways to bring my clients' visions to
+                life.
               </p>
               <p>
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
-                enjoyable experiences for users.{' '}
+                pretty .It's about solving problems and creating intuitive,
+                enjoyable experiences for users.
               </p>
               <p>
-                Whether I'm working on a website, mobile app, or other digital
-                product, I bring my commitment to design excellence and
-                user-centered thinking to every project I work on. I look
-                forward to the opportunity to bring my skills and passion to
-                your next project.
+                Whether I'm working on a website or other digital product, I
+                bring my commitment to design excellence and user-centered
+                thinking to every project I work on. I look forward to the
+                opportunity to bring my skills and passion to your next project.
               </p>
             </div>
           </div>
