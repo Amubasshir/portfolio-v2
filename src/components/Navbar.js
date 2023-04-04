1fc17cd92d1339a7526b58eeedf10777cda7ec62
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-04 16:54:57 +0600

    navbar redesigned

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,5 @@
-import { motion } from 'framer-motion';
 import Link from 'next/link';
-import {
-  GithubIcon,
-  LinkedInIcon,
-  MoonIcon,
-  SunIcon,
-  TwitterIcon,
-} from './Icons';
-import Logo from './Logo';
+import { MoonIcon, SunIcon } from './Icons';
 import useThemeSwitcher from './hooks/useThemeSwitcher';
 
 const CustomLink = ({ href, title, className = '' }) => {
@@ -22,63 +14,67 @@ const Navbar = () => {
   const [mode, setMode] = useThemeSwitcher();
 
   return (
-    <header className="w-full px-20 py-8 dark:bg-[#18191A] dark:text-light font-medium flex items-center justify-between">
-      <nav>
-        <CustomLink href="/" title="Home" className="mr-4 nav-link" />
-        <CustomLink
-          href="/projects"
-          title="Projects"
-          className="mx-4 nav-link"
+    <header className="w-full px-28 py-2 dark:bg-[#18191A] dark:text-light font-medium flex items-center justify-between">
+      <div>
+        <img
+          src="https://i.ibb.co/gSpSyt4/m-p-logo-black.png"
+          alt="logo"
+          width="100"
+          className="black-logo"
         />
-        <CustomLink href="/about" title="About" className="mx-4 nav-link" />
-        <CustomLink href="/contact" title="Contact" className="ml-4 nav-link" />
-      </nav>
-
-      <nav className="flex items-center justify-center flex-wrap">
-        <motion.a
-          href="https://twitter.com/lastmubasshir"
-          target="_blank"
-          whileTap={{ scale: 0.5 }}
-          whileHover={{ y: -3 }}
-          className="w-8 mx-3"
-        >
-          <TwitterIcon />
-        </motion.a>
-        <motion.a
-          href="https://www.linkedin.com/in/mubasshirahmed7/"
-          target="_blank"
-          whileTap={{ scale: 0.5 }}
-          whileHover={{ y: -3 }}
-          className="w-8 mx-3"
-        >
-          <LinkedInIcon />
-        </motion.a>
-        <motion.a
-          href="https://github.com/Amubasshir"
-          target="_blank"
-          whileTap={{ scale: 0.5 }}
-          whileHover={{ y: -3 }}
-          className="w-8 ml-3"
-        >
-          <GithubIcon />
-        </motion.a>
-
-        <button
-          onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
-          className={`ml-3 rounded-full flex items-center justify-center  p-1 ${
-            mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'
-          }`}
-        >
-          {mode === 'dark' ? (
-            <SunIcon className="fill-dark" />
-          ) : (
-            <MoonIcon className="fill-dark" />
-          )}
-        </button>
-      </nav>
-      <div className="absolute left-[50%] top-3 translate-x-[-50%]">
-        <Logo />
+        <img
+          src="https://i.ibb.co/bHShrbd/m-p-logo-white.png"
+          alt="logo"
+          width="100"
+          className="hidden dark:block"
+        />
       </div>
+
+      <nav className="left flex items-center">
+        <div>
+          <CustomLink
+            href="/"
+            title="Home"
+            className="mr-4  text-lg nav-link"
+          />
+          <CustomLink
+            href="/projects"
+            title="Projects"
+            className="mx-4 text-lg nav-link"
+          />
+          <CustomLink
+            href="/about"
+            title="About"
+            className="mx-4 text-lg nav-link"
+          />
+          <CustomLink
+            href="/contact"
+            title="Contact"
+            className="ml-4 text-lg nav-link"
+          />
+        </div>
+
+        <div>
+          <button
+            onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+            className={`ml-10 rounded-full flex items-center justify-center  p-1 ${
+              mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'
+            }`}
+          >
+            {mode === 'dark' ? (
+              <SunIcon className="fill-dark" />
+            ) : (
+              <MoonIcon className="fill-dark" />
+            )}
+          </button>
+        </div>
+      </nav>
+
+      <style jsx>{`
+        .black-logo {
+          display: ${mode === 'dark' ? 'none' : 'block'};
+        }
+      `}</style>
     </header>
   );
 };
